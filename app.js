0c7162f2d7a68c68ee1a45dc0b65eefd6e4f626e
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2018-11-19 21:35:26 +0100

    functionaliities implemented for showing error, displaying results etc

--- app.js
+++ app.js
@@ -1,67 +1,109 @@
-//listen for submit
-document.getElementById('loan-form').addEventListener('submit', function(e) {
+//Grab the fields
+const ageField = document.getElementById('age');
+const genderSelectField = document.getElementById('genderSelect');
+const heightField = document.getElementById('height');
+const weightField = document.getElementById('weight');
+const activitySelectField = document.getElementById('activitySelect');
+const bmrForm = document.getElementById('bmr-form');
+const bmrResult = document.getElementById('bmr-result');
+const cpmResult = document.getElementById('cpm-result');
+const resultsContainer = document.getElementById('results');
+const descriptionResult = document.getElementById('result-description');
+const loader = document.getElementById('loading');
+const hint = document.querySelector('.hint');
+
+bmrForm.addEventListener('submit', calculateDailyCaloricNeeds);
+ 
+function calculateDailyCaloricNeeds(e) {
+
   e.preventDefault();
+  resultsContainer.style.display = 'none';
 
-  document.querySelector('#results').style.display = 'none';
-  document.querySelector('#loading').style.display = 'block';
+  //Get the values from fields
+  const userAge = ageField.value;
+  const userGender = genderSelectField.options[genderSelectField.selectedIndex].text;
+  const userHeight = parseFloat(heightField.value);
+  const userWeight =  parseFloat(weightField.value);
+  const userActivityFactor = activitySelectField.value;
 
-  setTimeout(calculateResults, 2000)
-});
-
-//calc result
-function calculateResults() {
-
-  console.log('calculating')
-  const amount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
-
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value)*12;
-
-  //compute monthly payment
-  const x = Math.pow(1+calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest)/(x-1);
-
-  if(isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly*calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly*calculatedPayments) - principal).toFixed(2);
-
-    document.querySelector('#results').style.display = 'block';
-    document.querySelector('#loading').style.display = 'none';
-
-  } else {
-    showError('Please check your numbers');
+  if(!userAge || !userGender || !userHeight || !userWeight || !userActivityFactor) {
+    return handleShowingError('Something went wrong. Fill all the fields.');
   }
 
+  if(userAge < 10 || userHeight < 140 || userWeight < 40 || userAge > 110 || userHeight > 230 || userWeight > 200 ) {
+    return handleShowingError(`Are you really ${userAge} years old, ${userHeight} cm height and ${userWeight} kg weight?`)
+  }
+
+  //create a user object
+  let user = new Object();
+  user.age = userAge; 
+  user.gender = userGender,
+  user.height = userHeight,
+  user.weight = userWeight;
+  user.activity = userActivityFactor;
+
+  let bmrCalculated = calculateBmr(user);
+  let cpmCalculated = bmrCalculated*user.activity;
+  bmrResult.value = bmrCalculated;
+  cpmResult.value = cpmCalculated;
+
+  writeDescriptionResult(cpmCalculated);
+
+  handleDisplayingResults();
 
 }
 
-function showError(message) {
+function calculateBmr(person) {
+  const weightIndex = 9.99*person.weight;
+  const heightIndex = 6.25*person.height;
+  const ageIndex = 4.92*person.age;
 
-  document.querySelector('#results').style.display = 'none';
-  document.querySelector('#loading').style.display = 'none';
+  if(person.gender.toLowerCase='female') {
+    return weightIndex + heightIndex - ageIndex - 161;
+  }
 
-
-    const errorDiv = document.createElement('div');
-    errorDiv.className = 'alert alert-danger';
-    errorDiv.appendChild(document.createTextNode(message));
-
-  //get elements
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
-
-  card.insertBefore(errorDiv, heading); //you call it on a parent, you pass the element that you want to insert and then specify before which element.
-
-  //clear error after 3 sec
-  setTimeout(clearError, 3000) //takes 2 parameters, first is gonna be a function and second is the time.
+  return weightIndex + heightIndex - ageIndex + 5;
 }
 
-function clearError() {
-  document.querySelector('.alert').remove()
+function writeDescriptionResult(totalCpm) {
+  let htmlDescription = `
+  <ul>
+    <li class="text-muted">You need <b>${Math.round(totalCpm)}</b> Calories/day to maintain your weight</li>
+    <li class="text-success">You need <b>${Math.round(totalCpm)-300}</b> Calories/day to lose 0.5 kg per week</li>
+    <li class="text-danger">You need <b>${Math.round(totalCpm)+300}</b> Calories/day to gain 0.5 kg per week</li>
+  </ul>`
+
+  descriptionResult.innerHTML = htmlDescription
+  
+}
+
+function handleShowingError(message) {
+  let error = document.createElement('div');
+  error.className = 'alert alert-danger';
+  error.appendChild(document.createTextNode(message));
+  
+  loader.style.display = 'block';
+  hint.style.display = 'block';
+
+  setTimeout(function() {
+    document.querySelector('.card').insertBefore(error, bmrForm);
+    loader.style.display = 'none';
+    hint.style.display = 'none';
+  }, 1000)
+
+  setTimeout(function() {
+    error.remove();
+  }, 6000)
+
+}
+
+function handleDisplayingResults() {
+  loader.style.display = 'block';
+  hint.style.display = 'block';
+
+  setTimeout(function() {
+    loader.style.display = 'none';
+    hint.style.display = 'none';
+    resultsContainer.style.display = 'block';
+  }, 2000);
 }
